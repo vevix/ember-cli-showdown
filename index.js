484b1bbf73c89b4abf25be350b6e9aaea247ca5d
Thomas McNiven <hello@vevix.net>
2017-03-11 07:10:59 +1000

    Import showdown from NPM

--- index.js
+++ index.js
@@ -3,59 +3,20 @@
 
 var path = require('path');
 var funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
-
-function isFastBoot() {
-  return process.env.EMBER_CLI_FASTBOOT === 'true';
-}
 
 module.exports = {
   name: 'ember-cli-showdown',
 
-  blueprintsPath: function () {
-    return __dirname + '/blueprints';
+  treeForVendor: function() {
+    var showdownPath = path.join(path.dirname(require.resolve("showdown")), "..");
+    return new funnel(this.treeGenerator(showdownPath), {
+      destDir: 'showdown'
+    })
   },
 
   included: function showdownIncluded(app) {
     this._super.included.apply(this, arguments);
-
-    var host = this._findHost();
-
-    if (isFastBoot()) {
-      this.importFastBootDependencies(host);
-    } else {
-      this.importBrowserDependencies(host);
-    }
-  },
-
-  treeForVendor: function(vendorTree) {
-    var trees = [];
-
-    if (vendorTree) {
-      trees.push(vendorTree);
-    }
-
-    if (isFastBoot()) {
-      trees.push(funnel(path.join(__dirname, './assets'), {
-        files: ['fastboot-showdown.js']
-      }));
-    }
-
-    return mergeTrees(trees);
-  },
-
-  importFastBootDependencies: function(app) {
-    var pkg = require(path.join(app.project.root, 'package.json'));
-    var whitelist = pkg.fastbootDependencies;
-
-    if (!whitelist || whitelist && !~whitelist.indexOf('showdown')) {
-      throw new Error("[ember-cli-showdown] showdown is missing from package.json's fastbootDependencies.\nSee: https://github.com/ember-fastboot/ember-cli-fastboot#whitelisting-packages");
-    }
-
-    this.import('vendor/fastboot-showdown.js');
-  },
-
-  importBrowserDependencies: function(app) {
-    this.import(app.bowerDirectory + '/showdown/dist/showdown.js');
+    var showdownPath = path.join('vendor/showdown/dist');
+    app.import(path.join(showdownPath, 'showdown.js'));
   }
 };
